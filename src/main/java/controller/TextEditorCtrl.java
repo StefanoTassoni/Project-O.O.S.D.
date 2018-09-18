e01c7831939e6b4098c8694c470ba47116fd6067
Unknown <federiange@gmail.com>
2018-09-18 23:37:07 +0200

    test editor

---
+++ src/main/java/controller/TextEditorCtrl.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import controller.exception.ServiceException;
+import controller.utils.GUIUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import model.User;
+import model.service.LoginService;
+
+public class TextEditorCtrl {
+
+	@FXML Button textEditorBTN;
+	
+	@FXML protected void handleEditorButton(ActionEvent event) throws Exception {
+    	
+		GUIUtils guiUtils = GUIUtils.getInstance();
+		
+		DigitalLibrary.root = guiUtils.replaceSceneContent(DigitalLibrary.root, "view/AdminPage.fxml");		
+				
+    
+	}
+	
+	
+}
